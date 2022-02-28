9f6052ed65668f2195baf9598daa7a5b612dfacf
Kenneth Brandon <kbrandon319@gmail.com>
2022-02-28 15:06:52 -0500

    Path Update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-//initializing the variables and getting the DOM
-let adviceChange = document.getElementsByClassName("advice__dice-image");
-
-//this function retrieves the data from the api and then stores the data to be used and displayed in another function
-
 function adviceSlip() {
 
-  /*  assigning the api link */
   var url = `https://api.adviceslip.com/advice`;
 
   fetch(url)
@@ -15,26 +9,19 @@ function adviceSlip() {
 
     .then((data) => {
 
-        let displayID = document.getElementsByClassName('advice__id');
-        let displayText = document.getElementsByClassName('advice__quote');
+      let displayID = document.getElementsByClassName('advice__id');
+      let displayText = document.getElementsByClassName('advice__quote');
  
       /* created variables to store the data from the api */
       let slipId = data["slip"]["id"];
       let advice = data["slip"]["advice"];
 
-      //conecting the api variables to the dom
-
-      // adviceQuote.textContent = advice;
-
       console.log(slipId);
       console.log(advice);
-
-      
 
       displayID.innerHTML = `Advice ID #${slipId}`;
       displayText.innerHTML = advice;
 
-    
     });
 
   //error text is thrown if the data fetched cannot be retrieved
