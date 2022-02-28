0b21a9147bf1cbc163146ba015272d0692b2b473
Kenneth Brandon <kbrandon319@gmail.com>
2022-02-28 08:55:27 -0500

    Rendering Data from API to the Document

--- js/main.js
+++ js/main.js
@@ -1,46 +1,43 @@
 //initializing the variables and getting the DOM
-
-let adviceChange = document.getElementsByClassName('advice__dice-image');
-
+let adviceChange = document.getElementsByClassName("advice__dice-image");
 
 //this function retrieves the data from the api and then stores the data to be used and displayed in another function
 
+function adviceSlip() {
 
+  /*  assigning the api link */
+  var url = `https://api.adviceslip.com/advice`;
 
-function adviceSlip(){
-
-    /*  assigning the api link */
-    var url = `https://api.adviceslip.com/advice`;
-
-    fetch(url)
-    .then(response =>{
-        return response.json();
+  fetch(url)
+    .then((response) => {
+      return response.json();
     })
 
-    .then( data =>{
+    .then((data) => {
 
-        let adviceId = document.getElementsByClassName('.advice__text');
-        let adviceQuote = document.getElementsByClassName('advice__quote');
+        let displayID = document.getElementsByClassName('advice__id');
+        let displayText = document.getElementsByClassName('advice__quote');
+ 
+      /* created variables to store the data from the api */
+      let slipId = data["slip"]["id"];
+      let advice = data["slip"]["advice"];
 
-        /* created variables to store the data from the api */
-        let slipId = data['slip']['id'];
-        let advice = data['slip']['advice'];
+      //conecting the api variables to the dom
 
-        //conecting the api variables to the dom
+      // adviceQuote.textContent = advice;
 
-        
-        // adviceQuote.textContent = advice;
+      console.log(slipId);
+      console.log(advice);
 
-        console.log(slipId);
-       
+      
 
-        
-    })
-      //error text is thrown if the data fetched cannot be retrieved
+      displayID.innerHTML = `Advice ID #${slipId}`;
+      displayText.innerHTML = advice;
 
+    
+    });
 
-}
+  //error text is thrown if the data fetched cannot be retrieved
+};
 
-document.onload=adviceSlip();
-
-
+window.addEventListener('onload', adviceSlip());
